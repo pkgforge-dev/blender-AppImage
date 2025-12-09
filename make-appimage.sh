04d928966642b8f2d58991b3f5db7b48ddcf5c78
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-09 01:15:37 -0400

    package the thing

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,19 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q blender | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/scalable/apps/blender.svg
+export DESKTOP=/usr/share/applications/blender.desktop
+export DEPLOY_OPENGL=1
+export DEPLOY_VULKAN=1
+export DEPLOY_SYS_PYTHON=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /usr/bin/blender* /usr/share/blender
 
 # Additional changes can be done in between here
 
