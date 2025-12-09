4e7c7f192cc6d9397bdec8fa2f3c02704de3b4b4
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-09 10:49:15 -0400

    add `libdecor`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm blender
+pacman -Syu --noconfirm blender libdecor
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
